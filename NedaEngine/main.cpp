5f2b0c4a99c38b0bcae1ca6d441d80b808bbf2ac
Shahan Neda <shahan.neda@gmail.com>
2020-08-22 19:05:18 -0700

    Initial Commit

---
+++ NedaEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NedaEngine
+//
+//  Created by Shahan Nedadahandeh on 2020-08-22.
+//  Copyright © 2020 Shahan Nedadahandeh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
