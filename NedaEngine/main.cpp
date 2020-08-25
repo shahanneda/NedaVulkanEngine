00f3974e13b77cd12b47affe2d5e2a00fb1d04be
Shahan Neda <shahan.neda@gmail.com>
2020-08-25 12:40:21 -0700

    created image views

--- NedaEngine/main.cpp
+++ NedaEngine/main.cpp
@@ -65,6 +65,7 @@ private:
     VkQueue presentQueue;
     VkSwapchainKHR swapChain;
     std::vector<VkImage> swapChainImages;
+    std::vector<VkImageView> swapChainImageViews;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
 
@@ -91,6 +92,9 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageViews();
+        createGraphicsPipeline();
+        
     }
     
     void createLogicalDevice(){
@@ -256,6 +260,41 @@ private:
         swapChainExtent = extent;
         
     }
+    
+    void createImageViews(){
+        swapChainImageViews.resize(swapChainImages.size());
+        
+        for(size_t i = 0; i<swapChainImages.size(); i++){ // create a view for each image in the swap chain
+            
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            // just the defualt stuff
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+            
+            if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create image views!");
+            }
+        }
+        
+    }
+    
+    void createGraphicsPipeline(){
+        
+    }
+    
     
     void pickPhysicalDevice(){
 
@@ -494,8 +533,14 @@ private:
             DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
         }
         
+        for (auto imageView : swapChainImageViews) {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+        
         vkDestroySurfaceKHR(instance, surface, nullptr);
         vkDestroyInstance(instance, nullptr);
+        
+
         
         glfwDestroyWindow(window);
         glfwTerminate();
